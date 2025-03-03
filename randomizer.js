93f66a7204b725e31ba8399c9f2b38c0ce1c708d
Christoffer <chrna581@student.liu.se>
2025-03-03 17:31:55 +0100

    fixed randomizer

--- randomizer.js
+++ randomizer.js
@@ -1,8 +1,7 @@
 function randomizeProduct(){
     products = ["Audi", "Volvo", "Saab", "Bmw", "Opel"
     ]
-    productElement = document.getElementById("randomProduct")
     productIndex = Math.floor(Math.random()*products.length);
     product = products[productIndex];
-    productElement.getInnerHTML = "Random product: "+ product;
+    document.getElementById("randomProduct").innerHTML = "Random product: "+ product;
 }
